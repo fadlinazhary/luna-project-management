32222da7cdb1c49e336956c236624343cfa8fb8f
Fadlin Azhary <fadlinsiregar@gmail.com>
2025-10-11 13:49:57 +0700

    (chore) remove unused Livewire components

--- resources/views/livewire/counter.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <h1 class="text-2xl font-mono">This is counter!</h1>
-    <p class="text-lg">{{ $count }}</p>
-
-    <button wire:click="increment" class="px-10 bg-blue-400">+</button>
-    <button wire:click="decrement" class="px-10 bg-blue-400">-</button>
-</div>
