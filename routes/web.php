76c73ad0b32fb8a163714beeba6b017c0994117c
Fadlin Azhary <fadlinsiregar@gmail.com>
2025-10-17 21:35:17 +0700

    (chore) modify web routes and style

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,32 @@
 
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
+use App\Livewire\Pages\Dashboard;
+use App\Livewire\Pages\Projects;
+use App\Livewire\Pages\Settings\SettingsPage;
+use App\Livewire\Pages\Users;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', Login::class);
-Route::get('/register', Register::class);
+// TODO: Change into middleware route
+Route::middleware('guest')->group(function() {
+    Route::get('/login', Login::class)->name('login');
+    Route::get('/register', Register::class)->name('register');    
+});
 
-Route::get('/', function() {
-    return "You are logged in";
+Route::middleware('auth')->group(function() {
+    Route::get('/', Dashboard::class)->name('dashboard');
+    Route::get('/users', Users::class)->name('users');
+    Route::get('/projects', Projects::class)->name('projects');
+
+    Route::get('/settings', SettingsPage::class)->name('settings');
+
+    Route::get('/logout', function(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    });
 });
